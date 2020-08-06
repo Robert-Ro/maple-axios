c6d05634363d62dde72cecd4467b781946955d27
liutsing <liutsingluo@gmail.com>
2020-08-06 08:39:55 +0800

    feat: 响应数据支持 泛型

--- example/extend/app.ts
+++ example/extend/app.ts
@@ -1,46 +1,26 @@
 import axios from '../../src/index'
 
-axios({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hi'
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<User>>('/extend/user')
+    .then(res => res.data)
+    .catch(console.error)
+}
+
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.age) // 类型推断出
   }
-})
+}
 
-axios.request({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hello'
-  }
-})
-
-axios.get('/extend/get')
-
-axios.options('/extend/options')
-
-axios.delete('/extend/delete')
-
-axios.head('/extend/head')
-
-axios.post('/extend/post', { msg: 'post' })
-
-axios.put('/extend/put', { msg: 'put' })
-
-axios.patch('/extend/patch', { msg: 'patch' })
-
-axios({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hi'
-  }
-})
-
-axios('/extend/post', {
-  method: 'post',
-  data: {
-    msg: 'hello'
-  }
-})
+test()
